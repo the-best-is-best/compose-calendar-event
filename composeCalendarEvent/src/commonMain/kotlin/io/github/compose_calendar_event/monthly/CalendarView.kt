180f26548336616d047090a8ff6d88ea48e36f64
Michelle Raouf <michelle.raouf@52ndsolution.net>
2025-02-12 23:56:00 +0200

    Refactor CalendarView to add DatePickerDialog and enhance navigation
    -   Added `DatePickerDialog` to allow users to select a specific date.
    -   Implemented `showDatePicker` and `datePickerState` to control the dialog's visibility and state.
    -   Updated the logic to change the `selectedMonth` based on the chosen date from the `DatePickerDialog`.
    -   Added `onDateSelected` callback to pass the selected date to the parent.
    -   Updated the month/year text to be clickable and open the `DatePickerDialog`.
    -   Adjusted navigation buttons to update the `selectedMonth` correctly.
    -    Added `onMonthChanged` to `CalendarView` function.

--- composeCalendarEvent/src/commonMain/kotlin/io/github/compose_calendar_event/monthly/CalendarView.kt
+++ composeCalendarEvent/src/commonMain/kotlin/io/github/compose_calendar_event/monthly/CalendarView.kt
@@ -14,10 +14,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -36,15 +41,17 @@ import io.github.compose_calendar_event.utils.getDaysOfMonth
 import kotlinx.datetime.Clock
 import kotlinx.datetime.DatePeriod
 import kotlinx.datetime.DayOfWeek
+import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.minus
 import kotlinx.datetime.plus
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.datetime.todayIn
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun <T> CalendarView(
+fun CalendarView(
     isTwoWeeksSupport: Boolean = true,
     selectedDate: LocalDate = Clock.System.todayIn(TimeZone.currentSystemDefault()),
     eventDays: List<LocalDate> = emptyList(),
@@ -59,16 +66,13 @@ fun <T> CalendarView(
     displayItem: @Composable (LocalDate) -> Unit
 ) {
     var selectedMonth by remember {
-        mutableStateOf(
-            LocalDate(
-                selectedDate.year,
-                selectedDate.month,
-                1
-            )
-        )
+        mutableStateOf(LocalDate(selectedDate.year, selectedDate.month, 1))
     }
     var isMonthlyView by remember { mutableStateOf(true) }
     var currentHalf by remember { mutableStateOf(1) }
+    var showDatePicker by remember { mutableStateOf(false) }
+    val datePickerState = rememberDatePickerState()
+
     val daysOfMonth = getDaysOfMonth(selectedMonth, firstDayOfWeek)
     val totalWeeks = daysOfMonth.size / 7
     val firstHalfWeeks = totalWeeks / 2
@@ -78,37 +82,33 @@ fun <T> CalendarView(
         daysOfMonth.subList(splitIndex, daysOfMonth.size)
     )
 
-
     Column(
         modifier = Modifier.fillMaxHeight().fillMaxWidth().padding(16.dp)
-        .pointerInput(Unit) {
-            if (isTwoWeeksSupport) {
-                detectVerticalDragGestures { _, dragAmount ->
-                    if (isTwoWeeksSupport) {
-                        isMonthlyView = dragAmount >= 0
-                        currentHalf = 1
+            .pointerInput(Unit) {
+                if (isTwoWeeksSupport) {
+                    detectVerticalDragGestures { _, dragAmount ->
+                        if (isTwoWeeksSupport) {
+                            isMonthlyView = dragAmount >= 0
+                            currentHalf = 1
+                        }
                     }
                 }
             }
-        }
-        .nestedScroll(remember {
-            object : NestedScrollConnection {
-                override fun onPreScroll(
-                    available: Offset,
-                    source: NestedScrollSource
-                ): Offset {
-                    if (isTwoWeeksSupport) {
-                        isMonthlyView = available.y >= 0
-                        currentHalf = 1
+            .nestedScroll(remember {
+                object : NestedScrollConnection {
+                    override fun onPreScroll(
+                        available: Offset,
+                        source: NestedScrollSource
+                    ): Offset {
+                        if (isTwoWeeksSupport) {
+                            isMonthlyView = available.y >= 0
+                            currentHalf = 1
+                        }
+                        return Offset.Zero
                     }
-                    return Offset.Zero
                 }
-            }
-
-        })
+            })
     ) {
-
-
         Row(
             modifier = headerModifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
@@ -121,7 +121,6 @@ fun <T> CalendarView(
                 } else {
                     if (currentHalf == 2) {
                         currentHalf = 1
-
                     } else {
                         currentHalf = 2
                         selectedMonth = selectedMonth.minus(DatePeriod(months = 1))
@@ -134,26 +133,24 @@ fun <T> CalendarView(
                     contentDescription = "Previous"
                 )
             }
+
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Text(
                     text = "${selectedMonth.month.name} ${selectedMonth.year}",
                     style = headerTextStyle,
-                    modifier = Modifier.clickable {
-                        onDateSelected(selectedMonth)
-
-                    }
+                    modifier = Modifier
+                        .clickable { showDatePicker = true }
                         .padding(bottom = 8.dp)
                 )
-
             }
 
             if (isTwoWeeksSupport) {
                 Spacer(Modifier.weight(1f))
-
                 CalendarType(!isMonthlyView) {
                     isMonthlyView = !isMonthlyView
                 }
             }
+
             IconButton(onClick = {
                 if (isMonthlyView) {
                     selectedMonth = selectedMonth.plus(DatePeriod(months = 1))
@@ -175,6 +172,36 @@ fun <T> CalendarView(
             }
         }
 
+        // ✅ DatePickerDialog يظهر عند الضغط على الشهر
+        if (showDatePicker) {
+            DatePickerDialog(
+                onDismissRequest = { showDatePicker = false },
+                confirmButton = {
+                    TextButton(onClick = {
+                        val millis = datePickerState.selectedDateMillis
+                        if (millis != null) {
+                            val newDate = Instant.fromEpochMilliseconds(millis)
+                                .toLocalDateTime(TimeZone.currentSystemDefault()).date
+                            selectedMonth = LocalDate(newDate.year, newDate.month, 1)
+                            onMonthChanged(newDate)
+                            onDateSelected(newDate)
+
+                        }
+                        showDatePicker = false
+                    }) {
+                        Text("OK")
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = { showDatePicker = false }) {
+                        Text("Cancel")
+                    }
+                }
+            ) {
+                DatePicker(state = datePickerState)
+            }
+        }
+
         DayHeaders(firstDayOfWeek)
         MonthCalendar(
             days = if (isMonthlyView) daysOfMonth else splitDays[currentHalf - 1],
@@ -185,14 +212,14 @@ fun <T> CalendarView(
             currentDayColor = currentDayColor,
             eventDayColor = eventDayColor,
         )
+
         Spacer(Modifier.height(30.dp))
 
         LazyColumn {
             item {
                 displayItem(selectedDate)
-
             }
         }
-
     }
 }
+
